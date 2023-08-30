82b3f3d0a8f737b80209bcccb03fb4028652bdd5
Martin Hlavacek <martin@martinhlavacek.cz>
2023-08-30 21:13:07 +0200

    feat(alacritty, fish): update fish and alacritty config

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -103,5 +103,4 @@ function fish_prompt --description 'Write out the prompt'
         set  project "Fish"
     end
 
-    wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
 end
